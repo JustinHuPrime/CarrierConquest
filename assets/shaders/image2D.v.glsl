2e71a47d0bcaa3fbf4e88515e8299a676a1564a7
Justin Hu <justin.hu@alumni.ubc.ca>
2022-08-13 20:10:49 -0700

    fix typo

--- assets/shaders/image2D.v.glsl
+++ assets/shaders/image2D.v.glsl
@@ -22,7 +22,7 @@
 layout(location = 0) in vec2 pos;
 layout(location = 1) in vec2 texCoord;
 
-layout(location = 0)out vec2 texCoord_;
+layout(location = 0) out vec2 texCoord_;
 
 void main() {
   gl_Position = vec4(pos, 0.0, 1.0);
